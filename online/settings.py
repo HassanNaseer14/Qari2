aa2b919920c00bd4ff3141f5a655841948318051
Hassan <hasrayrah970@gmail.com>
2020-08-17 14:55:42 +0500

    Debug Tweaked

--- online/settings.py
+++ online/settings.py
@@ -27,9 +27,8 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 SECRET_KEY = '(5y6f)p^vv#kjeljt2!@^5ck_xo!ok$+g-ps@(9wq^v(@oe0u4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = ['onlineqari.herokuapp.com', 'localhost']
 
 
 # Application definition
